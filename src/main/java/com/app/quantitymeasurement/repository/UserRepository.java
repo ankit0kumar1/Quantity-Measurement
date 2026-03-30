56d9c8d34f244d723a8818ef59be873e04705d41
anikit0kumar1 <ankit620580@gmail.com>
2026-03-30 14:29:32 +0530

    [Ankit Kumar]: completed UC-18

---
+++ src/main/java/com/app/quantitymeasurement/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.app.quantitymeasurement.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.quantitymeasurement.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByMobileNumber(String mobileNumber);
+}
